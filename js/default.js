b240e9bba141fbc6e5c506f698112bf650115a81
Mayu-webプログラミング <g1627012@st.t-kougei.ac.jp>
2017-06-26 17:27:50 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,4 +1,20 @@
 
+
+var answer=0;
+
+for(var i = 1; i <= word; i++) {
+ if(i/3 === 0){
+  answer = Fizz;
+}
+else if(i/5 === 0){
+  answer = Buzz;
+}
+ if(i/5 === 0 && i/3 === 0){
+  answer = FizzBuzz;
+}
+}
+
 document.getElementById('form').onsubmit = function(){
- window.alert(document.getElementById('form').word.value);
+ window.alert(document.getElementById('form').answer.value);
+ return false;
 };
