e3b2939b78855eec5ce3d774699cbf2ac9ea5411
Mayu-webプログラミング <g1627012@st.t-kougei.ac.jp>
2017-06-26 18:35:48 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -2,9 +2,9 @@
  document.getElementById('form').onsubmit = function(){
  window.alert(document.getElementById('form').word.value);
  };
-var answer = document.getElementById('form').word.value;
+;
 
-for(var i = 1; i <= answer; i++) {
+for(var i = 1; i <= document.getElementById('form').word.value; i++) {
  var li = document.createElement('li');
  if(i % 5 == 0 && i % 3 == 0){
 li.textContent = 'FizzBuzz';
