3db1266e12a678b83cce7fc8bf47ec45f4806f2d
Mayu-webプログラミング <g1627012@st.t-kougei.ac.jp>
2017-06-26 18:16:21 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -7,20 +7,19 @@ var answer=0;
   return false;
  };
 
-for(var i = 1; i <= word; i++) {
- if(i/3 === 0){
-  answer = 'Fizz';
+for(var i = 1; i < word; i++) {
+ var li = document.createElement('li');
+ if(i % 5 == 0 && i % 3 == 0){
+li.textContent = 'FizzBuzz';
 }
-else if(i/5 === 0){
-  answer = 'Buzz';
+else if(i % 3 == 0){
+ li.textContent = 'Fizz';
 }
- 
- if(i/5 === 0 && i/3 === 0){
-  answer = 'FizzBuzz';
+else if(i % 5 == 0){
+ li.textContent = 'Buzz';
 }
- 
-var li = document.createElement('li');
- li.textContent = answer;
+else
+ li.textContent = i;
  document.getElementById('list').appendChild(li);
 }
 
